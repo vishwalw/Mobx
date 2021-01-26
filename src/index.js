26b5715c8c114bbd1220e25c27a02e10a36a45b9
vishwa_derasariya <vishwa.logicwind@gmail.com>
2021-01-26 21:49:07 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { observable, configure, action } from "mobx";
+import { observer } from "mobx-react";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+configure({ enforceActions: true });
+
+const appState = observable({
+  count: 0,
+  incCount: action("Increament counter", () => {
+    appState.count += 1;
+  }),
+  decCount: action("decreament counter", () => {
+    appState.count -= 1;
+  }),
+});
+
+const Counter = observer((props) => (
+  <section>
+    {props.appState.count}
+    <div>
+      <button onClick={props.appState.incCount}>Add</button>
+      <button onClick={props.appState.decCount}>Dec</button>
+    </div>
+  </section>
+));
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Counter appState={appState}></Counter>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
